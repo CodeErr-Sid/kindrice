3919b7d7b4afb3603714df52722162009867fd22
Rasmi142 <rasmioff@gmail.com>
2024-08-27 20:45:31 +0530

    lowgi page modified

--- frontend/src/Components/Howlowgi/Howlowgi.jsx
+++ frontend/src/Components/Howlowgi/Howlowgi.jsx
@@ -13,12 +13,12 @@ export default function Howlowgi() {
             <ol className='howlowgi-list' start={1}>
               <li>
                 <p className='howlowgi-second-para'>
-              1.  The<span> paddy variety, RNR,</span> naturally has a low glycemic index. 
+                The<span> paddy variety, RNR,</span> naturally has a low glycemic index. 
                 </p>
               </li>
               <li>
                 <p className='howlowgi-third-para'>
-              2.  our <span>signature boiling processing</span> method further lowers the glycemic index compared to raw or single-boiled rice
+                our <span>signature boiling processing</span> method further lowers the glycemic index compared to raw or single-boiled rice
                 </p>
               </li>
             </ol>
