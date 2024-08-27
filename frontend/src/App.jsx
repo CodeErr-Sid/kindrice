a59b464a2227a2e600877d51ab01dc80d80b2198
Rasmi142 <rasmioff@gmail.com>
2024-08-27 13:53:21 +0530

    hamburger error resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,15 @@ import Lowgi from './pages/Lowgi/Lowgi';
 import Contact from './pages/Contact/Contact';
 import ReturnPolicy from './pages/ReturnPolicy/ReturnPolicy';
 import Certificates from './pages/Certificates/Certificates';
+import Home2 from './pages/Home2/Home2';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/' element={<Home2 />} />
         <Route path='/story' element={<Story />} />
         <Route path='/impact' element={<Impact/>} />
         <Route path='/blog' element={<Blog/>} />
---
+++ frontend/src/pages/Home2/Home2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Herosection from '../../Components/Hero-section/Herosection'
+
+export default function Home2() {
+  return (
+    <>
+    <Navbar/>
+    <Herosection/>
+    </>
+  )
+}
