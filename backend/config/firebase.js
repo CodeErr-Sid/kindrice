4a246e98f4508aaaf823ea6fd5620768c2914a46
Akram-00 <aliakram9789@gmail.com>
2024-09-15 16:28:08 +0530

    firebase configured

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -9,7 +9,7 @@ const serviceAccount = {
     type: process.env.FIREBASE_TYPE,
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Handle newlines in private key
+    privateKey: process.env.FIREBASE_PRIVATE_KEY, // Handle newlines in private key
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     clientId: process.env.FIREBASE_CLIENT_ID,
     authUri: process.env.FIREBASE_AUTH_URI,
@@ -18,6 +18,7 @@ const serviceAccount = {
     clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
     universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
   };
+
 
 
 // Initialize Firebase Admin SDK
