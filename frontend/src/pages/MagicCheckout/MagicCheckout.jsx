00f5ef44949c6e3e329f611149ec59969084623f
Akram-00 <aliakram9789@gmail.com>
2024-09-27 11:42:06 +0530

    comming soon

--- frontend/src/pages/MagicCheckout/MagicCheckout.jsx
+++ frontend/src/pages/MagicCheckout/MagicCheckout.jsx
@@ -4,7 +4,7 @@ const MagicCheckout = () => {
   return (
     <div>
         <section className="w-full h-[100vh] flex justify-center items-center">
-            <h1 className='text-4xl'>Magic Checkout Yet to be Integrated</h1>
+            <h1 className='text-4xl'>COMING SOON</h1>
         </section>
     </div>
   )
