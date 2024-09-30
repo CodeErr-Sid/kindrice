d2517656c2c80811a6aad45e591a623fad2ee7f0
Akram-00 <aliakram9789@gmail.com>
2024-09-30 17:06:17 +0530

    Mongo uri is updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, { serverSelectionTimeoutMS: 10000, });
+    await mongoose.connect(MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
