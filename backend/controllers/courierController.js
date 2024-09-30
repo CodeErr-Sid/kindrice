57074a5a02b779f80e5faed471bbf5be41acf4c6
Akram-00 <aliakram9789@gmail.com>
2024-09-30 17:25:45 +0530

    shipping prices calculated

--- backend/controllers/courierController.js
+++ backend/controllers/courierController.js
@@ -2,38 +2,31 @@ import { shippingPrice } from "../config/shiprocket.js";
 import { fetchOrderId } from "./orderController.js";
 
 const getShippingPrice = async (req, res) => {
-    const { razorpay_order_id, addresses } = req.body;
-
-    
+    const { addresses } = req.body;
 
     try {
-        // Fetch order details using the razorpay_order_id
-        const order = await fetchOrderId(razorpay_order_id);
-
-        // Extract total price (in paise) and convert to rupees
-        const totalPrice = order.amount / 100;
-
-        // Extract total weight from the order notes
-        const totalWeight = order.notes.totalWeight;
-        // Iterate over each address and fetch shipping options
         const addressShippingInfo = await Promise.all(addresses.map(async (address) => {
-            const pincode = address.zipcode;
+            const shippingMethods = [
+                {
+                    "id": "1",
+                    "description": "Free shipping",
+                    "name": "Delivery within 5 days",
+                    "serviceable": true,
+                    "shipping_fee": 1000, // in paise. Here 1000 = 1000 paise, which equals to ₹10
+                    "cod": true,
+                    "cod_fee": 1000 // in paise. Here 1000 = 1000 paise, which equals to ₹10
+                },
+                {
+                    "id": "2",
+                    "description": "Standard Delivery",
+                    "name": "Delivered on the same day",
+                    "serviceable": true,
+                    "shipping_fee": 1000, // in paise. Here 1000 = 1000 paise, which equals to ₹10
+                    "cod": false,
+                    "cod_fee": 0 // in paise. Here 1000 = 1000 paise, which equals to ₹10
+                }
+            ]
 
-            // Call the shippingPrice function with required parameters (pincode, weight, price)
-            const shippingOptions = await shippingPrice(pincode, totalWeight, totalPrice);
-
-            // Map the shipping options to the required format
-            const shippingMethods = shippingOptions.map((option, index) => ({
-                id: String(index + 1),
-                description: "Standard Delivery",
-                name: `Delivery within ${option.estimated_delivery_days} days`,
-                serviceable: true,
-                shipping_fee: option.freight_charge * 100, // converting to paise
-                cod: false, // set to true if cod is available
-                cod_fee: 0 // cod_fee in paise, set to 0 if not available
-            }));
-
-            // Return the address object with shipping methods and capitalize the country
             return {
                 id: address.id,
                 zipcode: address.zipcode,
@@ -44,8 +37,7 @@ const getShippingPrice = async (req, res) => {
         }));
 
 
-        // Send the response with address shipping information
-        
+
         res.send({ addresses: addressShippingInfo });
 
     } catch (error) {
