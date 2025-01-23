69d0298d3f1e0b822566d229759eedc644846b1e
Akram-00 <aliakram9789@gmail.com>
2025-01-23 22:42:56 +0530

    Product Page Updated

--- backend/controllers/courierController.js
+++ backend/controllers/courierController.js
@@ -4,22 +4,22 @@ import { fetchOrderId } from "./orderController.js";
 const getShippingPrice = async (req, res) => {
     const { razorpay_order_id, addresses } = req.body;
 
-    console.log("body : "+req.body);
+    console.log("body : " + JSON.stringify(req.body));
 
     try {
         // Fetch order details using the razorpay_order_id
         const order = await fetchOrderId(razorpay_order_id);
 
-        console.log("Order : "+order);
+        console.log("Order : " + JSON.stringify(order));
 
         // Extract total price (in paise) and convert to rupees
         const totalPrice = order.amount / 100;
 
-        console.log("Total Price :"+totalPrice);
+        console.log("Total Price :" + JSON.stringify(totalPrice));
 
         // Extract total weight from the order notes
         const totalWeight = order.notes.totalWeight;
-        console.log("Total weight :"+totalWeight);
+        console.log("Total weight :" + totalWeight);
         // Iterate over each address and fetch shipping options
         const addressShippingInfo = await Promise.all(addresses.map(async (address) => {
             const pincode = address.zipcode;
@@ -28,15 +28,15 @@ const getShippingPrice = async (req, res) => {
             const shippingOptions = await shippingPrice(pincode, totalWeight, totalPrice);
 
             // Map the shipping options to the required format
-            const shippingMethods = shippingOptions.map((option, index) => ({
-                id: String(index + 1),
+            const shippingMethods = {
+                id: "1",
                 description: "Standard Delivery",
                 name: `Delivery within 10 - 15 days`,
                 serviceable: true,
-                shipping_fee: option.freight_charge * 100, // converting to paise
+                shipping_fee: shippingOptions.freight_charge * 100, // converting to paise
                 cod: false, // set to true if cod is available
                 cod_fee: 0 // cod_fee in paise, set to 0 if not available
-            }));
+            }
 
             // Return the address object with shipping methods and capitalize the country
             return {
